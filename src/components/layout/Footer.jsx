71a1ace191404c0d0630b9656c50ce6b374bd79e
snehachavan0923 <snehachavan0923@gmail.com>
2026-03-22 22:19:21 +0530

    Updated some things

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -55,7 +55,7 @@ const Footer = () => {
               </li>
               <li>
                 <Link to="/" className="hover:text-white transition-colors">
-                  API
+                  APIS
                 </Link>
               </li>
             </ul>
