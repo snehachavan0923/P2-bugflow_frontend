f46ce0d982397b369504d571f192a11a2c2259fb
snehachavan0923 <snehachavan0923@gmail.com>
2026-04-04 00:29:24 +0530

    updated

--- src/components/issue/IssueCard.jsx
+++ src/components/issue/IssueCard.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 
 const IssueCard = ({ issue, onStatusChange }) => {
   const handleStatusChange = (newStatus) => {
--- src/components/issue/IssueDetailsModal.jsx
+++ src/components/issue/IssueDetailsModal.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import Modal from '../common/Modal';
 
 const IssueDetailsModal = ({ isOpen, onClose, issue }) => {
